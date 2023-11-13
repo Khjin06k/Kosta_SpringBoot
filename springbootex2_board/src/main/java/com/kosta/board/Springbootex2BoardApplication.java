694305ab6da503d76c493990425b34de2686bdc8
gmlwls <khj4209kk@gmail.com>
2023-11-13 09:39:00 +0900

    springboot를 이용한 board 만들기

---
+++ springbootex2_board/src/main/java/com/kosta/board/Springbootex2BoardApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootex2BoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootex2BoardApplication.class, args);
+    }
+
+}
